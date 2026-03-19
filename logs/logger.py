d33123d74f5c95761504f04ca66ebe589ec45e44
moguraaa <demavivas.kyle.c@gmail.com>
2026-03-19 09:22:01 +0800

    Add logging functionality to connection tests

---
+++ logs/logger.py
@@ -0,0 +1,15 @@
+import os, logging
+
+def get_logger (name, log_file="sync.log"):
+    
+    LOG_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "logs", log_file)
+    os.makedirs(os.path.dirname(LOG_PATH), exist_ok=True)
+
+    logging.basicConfig(
+        format = "%(filename)s - %(asctime)s - %(levelname)s - %(message)s \n",
+        datefmt= "%m-%d-%Y %H:%M",
+        filename = LOG_PATH,
+        level = logging.DEBUG
+    )
+    
+    return logging.getLogger(name)
--- sync/testConnection.py
+++ sync/testConnection.py
@@ -3,9 +3,19 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from sqlalchemy import text
 from config.connections import MSSQL_ENGINE
+from logs.logger import get_logger
 
-with MSSQL_ENGINE.connect() as connection:
-    result = connection.execute(text("select * from user_account"))
+logger = get_logger(__name__)
+
+try:
+    with MSSQL_ENGINE.connect() as connection:
+        result = connection.execute(text("select * from user_account")).ftchall()
     
-    for row in result:
-        print("username: ", row.user_namefl)
+        for row in result:
+            print("username: ", row.user_namefl)
+            
+        logger.info("Fetch is successful!")      
+        
+except Exception as e:
+    logger.error(f": {e}")
+    print("Something went wrong. Check log files for details")
