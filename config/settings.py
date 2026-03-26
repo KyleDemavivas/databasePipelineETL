28713e98002fc6ba7822303132f2e0bc08556d51
moguraaa <demavivas.kyle.c@gmail.com>
2026-03-26 13:17:45 +0800

    removed sensitive files from repo

--- config/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-SYNC_INTERVAL = 60
-TIMEZONE = 'Asia/Manila'
-LOG_FILE = os.path.join(BASE_DIR, 'logs/sync.log')
-STATE_FILE = os.path.join(BASE_DIR, 'state/db_state.json')
-
-#WAG PALITAN BABANATAN KITA
